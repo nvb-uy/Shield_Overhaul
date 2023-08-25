4ab76bc8908e21f7b76314eda1b7f4d5d667fc4f
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-08-25 17:57:38 +0100

    add(shield bash entity discard logic)

--- src/main/java/elocindev/shield_overhaul/entity/ShieldBashEntity.java
+++ src/main/java/elocindev/shield_overhaul/entity/ShieldBashEntity.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.world.World;
 
@@ -20,11 +21,17 @@ public class ShieldBashEntity extends PersistentProjectileEntity {
     }
 
     @Override
+    protected void onBlockHit(BlockHitResult blockHitResult) {
+        this.discard();
+    }
+
+    @Override
     protected void onEntityHit(EntityHitResult entityHitResult) {
         super.onEntityHit(entityHitResult);
         if (entityHitResult.getEntity() instanceof LivingEntity entity && !entity.world.isClient()) {
             entity.addStatusEffect(new StatusEffectInstance(EffectRegistry.STUN_EFFECT, 20, 0, false, false, true));
         }
+        this.discard();
     }
 
     @Override
