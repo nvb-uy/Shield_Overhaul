1e35bc9382b9ca2ec571d858681cb61de9e4d275
nvb-uy <nvb-uy@no-reply.github.com>
2023-08-25 14:07:46 -0300

    fix(shield bash entity)

--- src/main/java/elocindev/shield_overhaul/entity/ShieldBashEntity.java
+++ src/main/java/elocindev/shield_overhaul/entity/ShieldBashEntity.java
@@ -7,6 +7,9 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.world.World;
 
@@ -24,11 +27,23 @@ public class ShieldBashEntity extends PersistentProjectileEntity {
         super.onEntityHit(entityHitResult);
         if (entityHitResult.getEntity() instanceof LivingEntity entity && !entity.world.isClient()) {
             entity.addStatusEffect(new StatusEffectInstance(EffectRegistry.STUN_EFFECT, 20, 0, false, false, true));
+
+            this.setRemoved(RemovalReason.DISCARDED); 
         }
     }
 
     @Override
     protected ItemStack asItemStack() {
         return null;
+    }
+
+    @Override
+    protected SoundEvent getHitSound() {
+        return new SoundEvent(new Identifier("null"));
+    }
+
+    @Override
+    protected void onBlockHit(BlockHitResult blockHitResult) {
+        this.setRemoved(RemovalReason.DISCARDED); 
     }
 }
