0863741bd238fb9f468412ddaafa8025170305e1
nvb-uy <nvb-uy@no-reply.github.com>
2023-08-25 11:15:35 -0300

    feat(working config)

--- src/main/java/elocindev/shield_overhaul/ShieldOverhaul.java
+++ src/main/java/elocindev/shield_overhaul/ShieldOverhaul.java
@@ -5,14 +5,20 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import elocindev.shield_overhaul.config.ConfigBuilder;
+import elocindev.shield_overhaul.config.ConfigEntries;
 import software.bernie.geckolib3.GeckoLib;
 
 public class ShieldOverhaul implements ModInitializer {
 	public static final String MODID = "shield_overhaul";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
+	public static ConfigEntries CONFIG;
+
 	@Override
 	public void onInitialize() {
+		CONFIG = ConfigBuilder.loadConfig();
+
 		GeckoLib.initialize();
 	}
 }
