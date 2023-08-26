fd039da99a113231c704b62855c216de9a1ecfe1
nvb-uy <nvb-uy@no-reply.github.com>
2023-08-26 13:23:19 -0300

    rem(unused imports)

--- src/main/java/elocindev/shield_overhaul/ShieldOverhaulClient.java
+++ src/main/java/elocindev/shield_overhaul/ShieldOverhaulClient.java
@@ -1,12 +1,7 @@
 package elocindev.shield_overhaul;
 
-import elocindev.shield_overhaul.entity.ShieldBashEntity;
 import elocindev.shield_overhaul.event.ShieldInteraction;
-import elocindev.shield_overhaul.registry.EntityRegistry;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.minecraft.client.render.entity.ProjectileEntityRenderer;
-import net.minecraft.util.Identifier;
 
 public class ShieldOverhaulClient implements ClientModInitializer {
     @Override
