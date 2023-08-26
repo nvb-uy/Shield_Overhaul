c871c2814854ccb32166a16687196387a8a73037
nvb-uy <nvb-uy@no-reply.github.com>
2023-08-26 13:23:32 -0300

    add(register renderers)

--- src/main/java/elocindev/shield_overhaul/ShieldOverhaulClient.java
+++ src/main/java/elocindev/shield_overhaul/ShieldOverhaulClient.java
@@ -1,12 +1,13 @@
 package elocindev.shield_overhaul;
 
 import elocindev.shield_overhaul.event.ShieldInteraction;
+import elocindev.shield_overhaul.registry.EntityRegistry;
 import net.fabricmc.api.ClientModInitializer;
 
 public class ShieldOverhaulClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        
+        EntityRegistry.registerRenderers();
         ShieldInteraction.bashingPacketRegistry();
     }
 }
