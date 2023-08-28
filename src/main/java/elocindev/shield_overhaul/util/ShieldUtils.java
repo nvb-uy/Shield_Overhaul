15ce86818f27d7b5e2b40dfe814c61e2a5261cfb
nvb-uy <nvb-uy@no-reply.github.com>
2023-08-28 02:49:48 -0300

    add(more shield utils)

--- src/main/java/elocindev/shield_overhaul/util/ShieldUtils.java
+++ src/main/java/elocindev/shield_overhaul/util/ShieldUtils.java
@@ -1,18 +1,28 @@
 package elocindev.shield_overhaul.util;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ShieldItem;
 import net.minecraft.world.World;
 
 public class ShieldUtils {
+    private static String key = "parry_window"; 
+
+    public static boolean isParrying(PlayerEntity player) {
+        if (!(player.getActiveItem().getItem() instanceof ShieldItem)) return false;
+        
+        return isParrying(player.getActiveItem(), player.world);
+    }
+
     public static boolean isParrying(ItemStack stack, World world) {
-        return stack.getNbt().getLong("parry_window") > world.getTime();
+        return stack.getNbt().getLong(key) > world.getTime();
     }
 
     public static void setParryWindow(ItemStack stack, World world, long ticks) {
-        stack.getNbt().putLong("parry_window", world.getTime() + ticks);
+        stack.getNbt().putLong(key, world.getTime() + ticks);
     }
 
     public static void resetParryWindow(ItemStack stack) {
-        stack.getNbt().putLong("parry_window", 0);
+        stack.getNbt().putLong(key, 0);
     }
 }
