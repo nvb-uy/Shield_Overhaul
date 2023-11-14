fca735423662acf8ad038d7c78b35db12546e778
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-11-14 16:16:56 +0000

    add(basic shield cooldown display working)

--- src/main/java/elocindev/shield_overhaul/mixin/CursorIconMixin.java
+++ src/main/java/elocindev/shield_overhaul/mixin/CursorIconMixin.java
@@ -1,8 +1,14 @@
 package elocindev.shield_overhaul.mixin;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
 import elocindev.shield_overhaul.ShieldOverhaul;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.option.AttackIndicator;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,12 +21,38 @@ public class CursorIconMixin {
     @Shadow
     private int scaledWidth;
     @Shadow private int scaledHeight;
+    @Shadow
+    private final MinecraftClient client;
 
-    private static final Identifier OVERLAY = new Identifier(ShieldOverhaul.MODID, "textures/gui/shield.png");
+    private static final Identifier SHIELD_ICONS = new Identifier(ShieldOverhaul.MODID, "textures/gui/shield.png");
+    private static final Identifier ICONS = new Identifier("textures/gui/icons.png");
 
-    @Inject(at = @At("HEAD"), method = "renderHotbar")
-    private void $shield_overhaul_renderHotbar(float tickDelta, DrawContext context, CallbackInfo ci) {
-        int i = this.scaledWidth / 2;
-        context.drawTexture(OVERLAY, i - 91-73, this.scaledHeight - 17, 0, 0, 60, 18, 60, 18);
+    public CursorIconMixin(MinecraftClient client) {
+        this.client = client;
+    }
+
+    @Inject(at = @At("TAIL"), method = "renderCrosshair")
+    private void $shield_overhaul_renderHotbar(DrawContext context, CallbackInfo ci) {
+        RenderSystem.blendFuncSeparate(GlStateManager.SrcFactor.ONE_MINUS_DST_COLOR, GlStateManager.DstFactor.ONE_MINUS_SRC_COLOR, GlStateManager.SrcFactor.ONE, GlStateManager.DstFactor.ZERO);
+        if (this.client.options.getAttackIndicator().getValue() == AttackIndicator.CROSSHAIR) {
+            float f = this.client.player.getItemCooldownManager().getCooldownProgress(Items.SHIELD, 0.0F);
+            boolean bl = false;
+            if (this.client.targetedEntity != null && this.client.targetedEntity instanceof LivingEntity && f >= 1.0F) {
+                bl = this.client.player.getAttackCooldownProgressPerTick() > 5.0F;
+                bl &= this.client.targetedEntity.isAlive();
+            }
+
+            int j = this.scaledHeight / 2 - 7 + 16;
+            int k = this.scaledWidth / 2 - 8;
+
+            if (bl) {
+                context.drawTexture(ICONS, k - 16, j - 16, 68, 94, 16, 16);
+            } else if (f < 1.0F) {
+                int l = (int)(f * 17.0F);
+                context.drawTexture(ICONS, k - 16, j - 16, 36, 94, 16, 4);
+                context.drawTexture(ICONS, k - 16, j - 16, 52, 94, l, 4);
+            }
+        }
+        RenderSystem.defaultBlendFunc();
     }
 }
