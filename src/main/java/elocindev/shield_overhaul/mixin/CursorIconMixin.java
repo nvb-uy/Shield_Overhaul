2778f9d885c8615fb70bc3fd527358476816229a
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-12-02 00:10:47 +0000

    fix(parry UI)

--- src/main/java/elocindev/shield_overhaul/mixin/CursorIconMixin.java
+++ src/main/java/elocindev/shield_overhaul/mixin/CursorIconMixin.java
@@ -3,11 +3,13 @@ package elocindev.shield_overhaul.mixin;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import elocindev.shield_overhaul.ShieldOverhaul;
+import elocindev.shield_overhaul.util.ShieldUtils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.option.AttackIndicator;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
@@ -42,37 +44,17 @@ public class CursorIconMixin {
                 bl &= this.client.targetedEntity.isAlive();
             }
 
+            ItemStack shield = this.client.player.getStackInHand(client.player.getActiveHand());
+            boolean isParrying = ShieldUtils.isParrying(shield, client.player.getWorld());
+
             int j = this.scaledHeight / 2 - 7 + 16;
             int k = this.scaledWidth / 2 - 8;
 
-            if (bl) {
+            if (isParrying) {
                 context.drawTexture(ICONS, k - 16, j - 14, 68, 94, 16, 16);
             } else if (f < 1.0F) {
-                int l = (int)(f * 17.0F);
-                context.drawTexture(ICONS, k - 16, j - 14, 36, 94, 16, 4);
-                context.drawTexture(ICONS, k - 16, j - 14, 52, 94, l, 4);
-            }
-        }
-        RenderSystem.defaultBlendFunc();
-
-        RenderSystem.blendFuncSeparate(GlStateManager.SrcFactor.ONE_MINUS_DST_COLOR, GlStateManager.DstFactor.ONE_MINUS_SRC_COLOR, GlStateManager.SrcFactor.ONE, GlStateManager.DstFactor.ZERO);
-        if (this.client.options.getAttackIndicator().getValue() == AttackIndicator.CROSSHAIR && this.client.player.isHolding(Items.SHIELD)) {
-            float f = this.client.player.getItemCooldownManager().getCooldownProgress(Items.SHIELD, 0.0F);
-            boolean bl = false;
-            if (this.client.targetedEntity != null && this.client.targetedEntity instanceof LivingEntity && f >= 1.0F) {
-                bl = this.client.player.getAttackCooldownProgressPerTick() > 5.0F;
-                bl &= this.client.targetedEntity.isAlive();
-            }
-
-            int j = this.scaledHeight / 2 - 7 + 16;
-            int k = this.scaledWidth / 2 - 8;
-
-            if (bl) {
-                context.drawTexture(ICONS, k - 16, j - 7, 68, 94, 16, 16);
-            } else if (f < 1.0F) {
-                int l = (int)(f * 17.0F);
-                context.drawTexture(ICONS, k - 16, j - 7, 36, 94, 16, 4);
-                context.drawTexture(ICONS, k - 16, j - 7, 52, 94, l, 4);
+                context.drawTexture(ICONS, k - 16, j - 14, 36, 94, 4, 4);
+                context.drawTexture(ICONS, k - 16, j - 14, 52, 94, 4, 4);
             }
         }
         RenderSystem.defaultBlendFunc();
