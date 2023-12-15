ee4d584c360eb77f54456d0d13158a4e85dc4fcf
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2023-12-15 23:33:50 +0000

    rem(sneak to parry; reverting to right click to parry)

--- src/main/java/elocindev/shield_overhaul/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/elocindev/shield_overhaul/mixin/ServerPlayerInteractionManagerMixin.java
@@ -6,6 +6,8 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.ChunkPos;
 import org.spongepowered.asm.mixin.Mixin;
@@ -33,6 +35,29 @@ public class ServerPlayerInteractionManagerMixin {
     public void interactItem(ServerPlayerEntity player, World world, ItemStack stack, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
         if (player.hasStatusEffect(EffectRegistry.STUN_EFFECT)) {
             cir.setReturnValue(ActionResult.FAIL);
+        }
+
+        if (!(((LivingEntity) (Object) this) instanceof PlayerEntity playerEntity) || !(playerEntity.getStackInHand(playerEntity.getActiveHand()).getItem() instanceof ShieldItem)) return;
+
+        Hand shieldHand = playerEntity.getActiveHand();
+        ItemStack shield = playerEntity.getStackInHand(shieldHand);
+
+        ConfigEntries config = ShieldOverhaul.CONFIG;
+
+        if (config.enable_parrying && (shield.getItem() instanceof ShieldItem) && !playerEntity.getItemCooldownManager().isCoolingDown(shield.getItem()) && playerEntity.isBlocking()) {
+            if (config.blacklisted_shields.contains(Registries.ITEM.getId(shield.getItem()).toString()))
+                return;
+
+            if (ShieldUtils.isParrying(shield, playerEntity.getWorld())) {
+                // This will reset the parry if the player happens to use the shield why the parry window is active
+                // To prevent spam clicking for permanent parry
+                ShieldUtils.resetParryWindow(shield);
+                if (config.enable_parry_abuse_prevention) {
+                    playerEntity.getItemCooldownManager().set(shield.getItem(), (int)(config.parry_abuse_cooldown_secs * 20));
+                }
+            } else {
+                ShieldUtils.setParryWindow(shield, playerEntity.getWorld(), (int)(config.parry_window_secs * 20));
+            }
         }
     }
 }
