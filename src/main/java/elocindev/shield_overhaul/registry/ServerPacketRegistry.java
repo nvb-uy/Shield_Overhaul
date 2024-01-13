851a0f9bc13bdf5a3665b363fcb9d5f584bdac48
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2024-01-13 03:42:31 +0000

    rem(unncessary packet)

--- src/main/java/elocindev/shield_overhaul/registry/ServerPacketRegistry.java
+++ src/main/java/elocindev/shield_overhaul/registry/ServerPacketRegistry.java
@@ -2,7 +2,6 @@ package elocindev.shield_overhaul.registry;
 
 import elocindev.shield_overhaul.ShieldOverhaul;
 import elocindev.shield_overhaul.networking.ParryEffectC2SPacket;
-import elocindev.shield_overhaul.networking.SendAnimationC2SPacket;
 import elocindev.shield_overhaul.networking.ShieldBashC2SPacket;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.util.Identifier;
@@ -10,11 +9,9 @@ import net.minecraft.util.Identifier;
 public class ServerPacketRegistry {
     public static final Identifier SHIELD_BASH_PACKET = new Identifier(ShieldOverhaul.MODID, "shield_bash_packet");
     public static final Identifier PARRY_EFFECT_PACKET = new Identifier(ShieldOverhaul.MODID, "parry_effect_packet");
-    public static final Identifier SEND_ANIMATION_PACKET = new Identifier(ShieldOverhaul.MODID, "send_animation_packet");
 
     public static void registerC2SPackets() {
         ServerPlayNetworking.registerGlobalReceiver(SHIELD_BASH_PACKET, ShieldBashC2SPacket::receive);
         ServerPlayNetworking.registerGlobalReceiver(PARRY_EFFECT_PACKET, ParryEffectC2SPacket::receive);
-        ServerPlayNetworking.registerGlobalReceiver(SEND_ANIMATION_PACKET, SendAnimationC2SPacket::receive);
     }
 }
