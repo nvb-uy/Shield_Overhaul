96df6dd8f6201d29fb8b319f5e638c00d317ed6c
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2024-01-21 03:10:05 +0000

    add(parry animation to sprite sheet)

--- src/main/java/elocindev/shield_overhaul/mixin/CursorIconMixin.java
+++ src/main/java/elocindev/shield_overhaul/mixin/CursorIconMixin.java
@@ -60,10 +60,11 @@ public class CursorIconMixin {
 
             // Parry shield
             if (bl) {
-                context.drawTexture(SHIELD_ICONS, k + 17, j - 17, 0, 0, 16, 18);
+                context.drawTexture(SHIELD_ICONS, k + 15, j - 17, 0, 0, 16, 18);
             } else if (f < 1.0F) {
                 int l = (int)(f * 16.0f);
-                context.drawTexture(SHIELD_ICONS, k + 17, j - 17, 0, 0, 16, 18);
+                context.drawTexture(SHIELD_ICONS, k + 15, j - 17, 0, 0, 16, 18);
+                context.drawTexture(SHIELD_ICONS, k + 16, j - 17, 16, 0, l, 18);
             }
         }
         RenderSystem.defaultBlendFunc();
