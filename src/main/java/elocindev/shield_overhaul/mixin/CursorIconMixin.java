64cdedbd5776870daf84ba8dc2951fc034e00585
JosephRepo <141879006+JRepo42@users.noreply.github.com>
2024-01-23 00:04:02 +0000

    fix(gui alignment etc)

--- src/main/java/elocindev/shield_overhaul/mixin/CursorIconMixin.java
+++ src/main/java/elocindev/shield_overhaul/mixin/CursorIconMixin.java
@@ -51,11 +51,11 @@ public class CursorIconMixin {
 
             // Bash shield
             if (bl) {
-                context.drawTexture(SHIELD_ICONS, k - 16, j - 19, 0, 56, 16, 18);
+                context.drawTexture(SHIELD_ICONS, k - 13, j - 19, 0, 56, 16, 18);
             } else if (f < 1.0F) {
                 int l = (int)(f * 16.0f);
-                //context.drawTexture(SHIELD_ICONS, k - 16, j - 0, 0, 56, 16, 18);
-                context.drawTexture(SHIELD_ICONS, k - 16, j - 19, 0, 56, 16, 18);
+                context.drawTexture(SHIELD_ICONS, k - 13, j - 19, 0, 56, 16, 18);
+                context.drawTexture(SHIELD_ICONS, k - 13, j - 19, 16, 56, l, 18);
             }
 
             // Parry shield
